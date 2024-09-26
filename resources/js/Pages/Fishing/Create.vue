493e86a61f58eb93679ae66a8a7b72053b8c19ba
Masaki Nishino <nishino@seagullmark.com>
2024-09-26 14:28:38 +0900

    Update Create.vue

--- resources/js/Pages/Fishing/Create.vue
+++ resources/js/Pages/Fishing/Create.vue
@@ -1,5 +1,5 @@
 <template>
-  <form>
+  <form @submit.prevent="submit">
     <div class="space-y-12">
       <div class="border-b border-gray-900/10 pb-12">
         <h2 class="text-base font-semibold leading-7 text-gray-900">Fishing registration</h2>
@@ -12,7 +12,7 @@
             <div class="mt-2">
               <input
                 type="date"
-                name="fishing_date"
+                v-model="form.fishing_date"
                 id="fishing_date"
                 autocomplete="off"
                 class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -27,7 +27,7 @@
             <div class="mt-2">
               <textarea
                 id="comment"
-                name="comment"
+                v-model="form.comment"
                 rows="3"
                 class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
@@ -41,19 +41,35 @@
             <div
               class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10"
             >
-              <div class="text-center">
-                <PhotoIcon class="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
+              <div class="text-center" @dragover.prevent @drop.prevent="handleDrop">
+                <div v-if="!form.file">
+                  <!-- 画像がセットされていない場合のみ表示 -->
+                  <PhotoIcon class="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
+                </div>
                 <div class="mt-4 flex text-sm leading-6 text-gray-600">
                   <label
                     for="file-upload"
                     class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
                   >
                     <span>Upload a file</span>
-                    <input id="file-upload" name="file-upload" type="file" class="sr-only" />
+                    <input
+                      id="file-upload"
+                      name="file-upload"
+                      type="file"
+                      class="sr-only"
+                      @change="handleFileChange"
+                    />
                   </label>
                   <p class="pl-1">or drag and drop</p>
                 </div>
                 <p class="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
+                <!-- 追加: セットされた画像を表示 -->
+                <img
+                  v-if="form.file"
+                  :src="createObjectURL(form.file)"
+                  class="mt-2 max-w-full h-auto"
+                  alt="Selected file preview"
+                />
               </div>
             </div>
           </div>
@@ -69,7 +85,7 @@
             <div class="mt-2">
               <input
                 type="text"
-                name="place"
+                v-model="form.place"
                 id="place"
                 autocomplete="off"
                 class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -88,9 +104,10 @@
               <div class="flex items-center gap-x-3">
                 <input
                   id="fly-fishing"
-                  name="fishing_type"
+                  v-model="form.fishing_type"
                   type="radio"
                   class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                  value="fly"
                 />
                 <label for="fly-fishing" class="block text-sm font-medium leading-6 text-gray-900"
                   >fly fishing</label
@@ -99,9 +116,10 @@
               <div class="flex items-center gap-x-3">
                 <input
                   id="lure-fishing"
-                  name="fishing_type"
+                  v-model="form.fishing_type"
                   type="radio"
                   class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                  value="lure"
                 />
                 <label for="lure-fishing" class="block text-sm font-medium leading-6 text-gray-900"
                   >lure fishing</label
@@ -110,9 +128,10 @@
               <div class="flex items-center gap-x-3">
                 <input
                   id="bait-fishing"
-                  name="fishing_type"
+                  v-model="form.fishing_type"
                   type="radio"
                   class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                  value="bait"
                 />
                 <label for="bait-fishing" class="block text-sm font-medium leading-6 text-gray-900"
                   >bait fishing</label
@@ -125,8 +144,11 @@
     </div>
 
     <div class="mt-6 flex items-center justify-end gap-x-6">
-      <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
+      <button type="button" class="text-sm font-semibold leading-6 text-gray-900" @click="reset">
+        Cancel
+      </button>
       <button
+        @click="store"
         type="submit"
         class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
       >
@@ -137,5 +159,59 @@
 </template>
 
 <script setup>
+import { computed } from 'vue'
+import { useForm, router } from '@inertiajs/vue3'
 import { PhotoIcon, UserCircleIcon } from '@heroicons/vue/24/solid'
+import NProgress from 'nprogress'
+
+router.on('progress', (event) => {
+  if (event.detail.progress.percentage) {
+    NProgress.set((event.detail.progress.percentage / 100) * 0.9)
+  }
+})
+
+const props = defineProps({
+  fishing: Object
+})
+
+const form = useForm({
+  fishing_date: new Date().toISOString().split('T')[0], // 今日の日付を設定
+  comment: '',
+  place: '',
+  fishing_type: 'fly',
+  file: null
+})
+
+const handleFileChange = (event) => {
+  const file = event.target.files[0]
+  if (file) {
+    form.file = file
+  }
+}
+
+const handleDrop = (event) => {
+  const file = event.dataTransfer.files[0]
+  if (file) {
+    form.file = file
+  }
+}
+
+const createObjectURL = (file) => {
+  return URL.createObjectURL(file)
+}
+
+const store = () => {
+  form.post(route('fishing.store'), {
+    preserveScroll: true,
+    onSuccess: () => {
+      form.reset()
+      form.clearErrors()
+    }
+  })
+}
+
+const reset = () => {
+  form.reset()
+  form.clearErrors()
+}
 </script>
