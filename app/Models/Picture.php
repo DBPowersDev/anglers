a705328da3bfe9e6b5a246269ddd0ef66a7a3862
Masaki Nishino <nishino@seagullmark.com>
2024-10-03 09:03:51 +0900

    Update Picture.php

--- app/Models/Picture.php
+++ app/Models/Picture.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use GearboxSolutions\EloquentFileMaker\Database\Eloquent\FMModel;
-use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Picture extends FMModel
 {
