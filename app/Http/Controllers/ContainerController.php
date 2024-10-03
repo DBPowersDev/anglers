41fb3d728c00353f322e92c74dc0714a438d6822
Masaki Nishino <nishino@seagullmark.com>
2024-10-03 13:45:26 +0900

    Update ContainerController.php

--- app/Http/Controllers/ContainerController.php
+++ app/Http/Controllers/ContainerController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
 use Illuminate\View\ViewException;
+use Illuminate\Support\Facades\Http;
 
 class ContainerController extends Controller
 {
@@ -17,7 +18,7 @@ class ContainerController extends Controller
         'svg'  => 'image/svg+xml',
     );
 
-    public function getImage(Request $request)
+    public function getImageBadKnowhow(Request $request)
     {
         $path = Crypt::decryptString($request->route('path'));
         $info = pathinfo($path);
@@ -42,5 +43,30 @@ class ContainerController extends Controller
         $content_type = $this->mimes[$ext];
         header('Content-Type: ' . $content_type);
         echo $img;
+    }
+
+    public function getImage(Request $request)
+    {
+        $path = Crypt::decryptString($request->route('path'));
+        $info = pathinfo($path);
+        $ext = explode('?', strtolower(@$info['extension']));
+        $ext = $ext[0];
+
+        // クッキーの管理のためにCookieJarを作成
+        $cookieJar = \GuzzleHttp\Cookie\CookieJar::fromArray([], ''); // 空のクッキーを作成
+
+        // curlの代わりにHttpファサードを使用
+        $response = Http::withOptions([
+            'follow_redirects' => true, // リダイレクトを追従
+            'cookies' => $cookieJar, // CookieJarを指定
+        ])->get($path);
+
+        if ($response->failed()) {
+            return response()->json(['error' => $response->body()], 500); // エラーをJSON形式で返す
+        }
+
+        // Content-Typeを設定してレスポンスを返す
+        $content_type = $this->mimes[$ext];
+        return response($response->body(), 200)->header('Content-Type', $content_type);
     }
 }
