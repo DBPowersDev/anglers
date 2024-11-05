f809cf3ca852e2af81fdcd37d4a6d1a4f7aecc41
Masaki Nishino <nishino@seagullmark.com>
2024-11-05 09:19:23 +0900

    Update ContentSecurityPolicy.php

--- app/Http/Middleware/ContentSecurityPolicy.php
+++ app/Http/Middleware/ContentSecurityPolicy.php
@@ -23,9 +23,12 @@ class ContentSecurityPolicy
 
         // app.blade.phpで生成したノンスを取得
         $nonce = $request->session()->get('csp_nonce', ''); // セッションから取得
-        $viteServerHost = env('VITE_SERVER_HOST');
+
         // heredocをダブルクオートで囲む
         if (env('APP_ENV') === 'local') {
+
+            $viteServerHost = env('VITE_SERVER_HOST');
+
             // 開発環境では style-src に 'unsafe-inline' を許可
             $csp = <<<END
             default-src 'self';
@@ -35,6 +38,7 @@ class ContentSecurityPolicy
             img-src 'self' data:;
             END;
         } else {
+
             // 本番環境では style-src に 'unsafe-inline' を使用しない
             $csp = <<<END
             default-src 'self';
